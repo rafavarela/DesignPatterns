4867f55b2107f93c89d99d412f4b57f956cbab3e
Rafael Varela <rafael.varela@trip-arc.com>
2025-09-19 04:24:40 -0500

    Decorator pattern example: HTML text decorator

---
+++ ConsoleApp1/Structural/Decorator/TextEditorExample/Decorators/ItalicDecorator.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp1.Structural.Decorator.TextEditorExample.Decorators;
+
+public class ItalicDecorator(IText text) : TextDecorator(text)
+{
+    public override string Render()
+    {
+        return "<i>" + base.Render() + "</i>";
+    }
+}
